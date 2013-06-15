b57cf3bb21b691c32c9eec7da8a6130e91070e6f
Samuel Pun <spongeylondon@gmail.com>
2013-06-15 22:08:46 +0100

    catch exception when searching empty string
    by using flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,25 +1,32 @@
 class MainController < ApplicationController
   def index
+
     flickr = Flickr.new
-    if params[:text].nil?
-      #render :text => '<h2>Please enter a search string</h2>'
-    else
-      begin
-        # @photos will be a collection of photos returned as a PhotoCollection a subclass
-        # of Array
-        @photos = flickr.photos(:text => params[:text], :per_page =>'5', :sort => 'relevance')
+    respond_to do |format|
+      if params[:text].blank?
 
-        #youtube search
-        client = Google::APIClient.new(:key => YTkey::GOOGLE_API_KEY, :authorization => nil)
-        youtube = client.discovered_api(YTkey::GOOGLE_APP_1, YTkey::GOOGLE_APP_VERSION_1)
+        flash.now[:notice] = 'Please enter a search string' if params[:searching]
+        format.html{ render :action => 'index'}
+      else
+        begin
+          # @photos will be a collection of photos returned as a PhotoCollection subclass
+          # of Array
+          @photos = flickr.photos(:text => params[:text], :per_page =>'5', :sort => 'relevance')
 
-        @youtubevideos = client.execute!(:api_method => youtube.search.list, :parameters => {:q => params[:text], :part => 'snippet', :type => 'video', :videoEmbeddable => 'true'})
+          #youtube search
+          client = Google::APIClient.new(:key => YTkey::GOOGLE_API_KEY, :authorization => nil)
+          youtube = client.discovered_api(YTkey::GOOGLE_APP_1, YTkey::GOOGLE_APP_VERSION_1)
 
-        render 'main/display'
+          @youtubevideos = client.execute!(:api_method => youtube.search.list, :parameters => {:q => params[:text], :part => 'snippet', :type => 'video', :videoEmbeddable => 'true'})
+
+          format.html{render :action => 'display'}
+          #render 'main/display'
+        end
       end
     end
   end
 
   def display
+
   end
 end
--- app/models/clipboard_image.rb
+++ app/models/clipboard_image.rb
@@ -1,4 +1,6 @@
 class ClipboardImage < ActiveRecord::Base
 	attr_accessible :id, :url, :user_id
+  validates :url, :user_id, :source, presence: true
+  validates :url, :uniqueness => { :scope => :user_id }
 	#attr_reader :id
 end
