844792f41a544fa06ff4828053d07fceec09b6d0
James Hume <james.hume@gmail.com>
2013-08-04 11:49:38 +0100

    Deleting old db migrations, no longer needed, that were causing migrate eerrors when building from scratch

--- db/migrate/20130619213623_remove_user_id_from_clipboard_images.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromClipboardImages < ActiveRecord::Migration
-  def up
-    remove_column :clipboard_images, :user_id
-      end
-
-  def down
-    add_column :clipboard_images, :user_id, :integer
-  end
-end
